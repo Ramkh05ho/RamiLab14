fabc919525713a0a3f461cb85f1438323bdf8212
Ramkh05ho <rkhosho@cps.edu>
2023-07-13 13:31:50 -0500

    RamiLab14

---
+++ src/RamiLab14.cpp
@@ -0,0 +1,82 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/ramikhosho/RamiLab14/src/RamiLab14.ino"
+void setup();
+void loop();
+void resetDisplay();
+void isr();
+#line 1 "/Users/ramikhosho/RamiLab14/src/RamiLab14.ino"
+#define PIN D7
+#define IPIN D6
+
+#include "oled-wing-adafruit.h"
+
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+volatile uint16_t counter = 0;
+volatile bool interruptOccured = false;
+volatile bool InterruptsOn = true;
+volatile bool buttonState = false;
+
+OledWingAdafruit display;
+
+void setup()
+{
+  display.setup();
+  // display.setTextSize(1);
+  // display.setTextColor(WHITE);
+  // resetDisplay();
+  // display.display();
+  pinMode(PIN, INPUT_PULLUP);
+  pinMode(IPIN, INPUT_PULLUP);
+  attachInterrupt(PIN, isr, FALLING);
+}
+void loop()
+{
+  display.loop();
+
+  if (interruptOccured)
+  {
+    counter++;
+    resetDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.println(counter);
+    display.display();
+    Serial.println(counter);
+    display.display();
+    interruptOccured = false;
+  }
+  if ((!digitalRead(IPIN)))
+  {
+    buttonState = true;
+  }
+  else if (buttonState)
+  {
+    InterruptsOn = !InterruptsOn;
+    if (InterruptsOn)
+    {
+      interrupts();
+    }
+    else
+    {
+      noInterrupts();
+    }
+    buttonState = false;
+  }
+}
+void resetDisplay()
+{
+  display.clearDisplay();
+  display.setCursor(0, 0);
+}
+void isr()
+{
+  noInterrupts();
+  interruptOccured = true;
+  interrupts();
+}
